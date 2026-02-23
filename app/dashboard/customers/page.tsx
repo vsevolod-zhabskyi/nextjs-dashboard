971db71e42c818eecc08cff6196fd26e2a7e0f68
Vsevolod Zhabskyi <vsevolod_zhabskyi@epam.com>
2026-02-23 19:17:04 +0200

    Create Next.js Dashboard app

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      Customers page
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      Invoices page
+    </div>
+  );
+};
+
+export default Page;
