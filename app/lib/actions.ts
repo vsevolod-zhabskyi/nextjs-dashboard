013c008886b048740487ed475330918deea7177b
Vsevolod Zhabskyi <vsevolod_zhabskyi@epam.com>
2026-02-27 12:23:31 +0200

    Fix delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -113,9 +113,6 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (e) {
     console.error(e)
-    return {
-      message: 'Database Error: Failed to delete invoice'
-    }
   }
   revalidatePath('/dashboard/invoices');
 }
