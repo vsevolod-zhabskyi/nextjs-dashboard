1a7b45794f94426a7f1c9b2f4e9be312883cb5bb
Vsevolod Zhabskyi <vsevolod_zhabskyi@epam.com>
2026-02-27 17:18:22 +0200

    Fix edit invoice form do preserve user input on error add display error messages

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,8 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import {State, updateInvoice} from '@/app/lib/actions';
-import {useActionState} from 'react';
+import { State, updateInvoice } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,7 +19,15 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const initialState: State = { message: null, errors: {}, values: {} };
+  const initialState: State = {
+    message: null,
+    errors: {},
+    values: {
+      customerId: invoice.customer_id,
+      amount: String(invoice.amount),
+      status: invoice.status,
+    }
+  };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
@@ -36,7 +44,9 @@ export default function EditInvoiceForm({
               id="customer"
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue={invoice.customer_id}
+              aria-describedby="customer-error"
+              defaultValue={state.values?.customerId ?? ""}
+              key={state.values?.customerId ?? 'empty'}
             >
               <option value="" disabled>
                 Select a customer
@@ -48,6 +58,15 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map(error => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))
+            }
           </div>
         </div>
 
@@ -63,12 +82,22 @@ export default function EditInvoiceForm({
                 name="amount"
                 type="number"
                 step="0.01"
-                defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
+                defaultValue={state.values?.amount ?? ""}
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id="amount-error" aria-live="polite" aria-atomic>
+            {state.errors?.amount &&
+              state.errors.amount.map(error => (
+                <p className="text-sm text-red-500 mt-2" key={error}>
+                  {error}
+                </p>
+              ))
+            }
           </div>
         </div>
 
@@ -85,8 +114,9 @@ export default function EditInvoiceForm({
                   name="status"
                   type="radio"
                   value="pending"
-                  defaultChecked={invoice.status === 'pending'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
+                  defaultChecked={state.values?.status === 'pending'}
                 />
                 <label
                   htmlFor="pending"
@@ -95,14 +125,16 @@ export default function EditInvoiceForm({
                   Pending <ClockIcon className="h-4 w-4" />
                 </label>
               </div>
+
               <div className="flex items-center">
                 <input
                   id="paid"
                   name="status"
                   type="radio"
                   value="paid"
-                  defaultChecked={invoice.status === 'paid'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
+                  defaultChecked={state.values?.status === 'paid'}
                 />
                 <label
                   htmlFor="paid"
@@ -113,8 +145,18 @@ export default function EditInvoiceForm({
               </div>
             </div>
           </div>
+          <div id="status-error" aria-live="polite" aria-atomic>
+            {state.errors?.status &&
+              state.errors.status.map(error => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))
+            }
+          </div>
         </fieldset>
       </div>
+
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
